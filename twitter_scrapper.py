034891622b2328d5bfe0d68e44541d3de5788c1d
CalmDownKarm <karmanyaaggarwal@gmail.com>
2021-11-24 23:05:08 -0500

    kicking off data collection

--- twitter_scrapper.py
+++ twitter_scrapper.py
@@ -2,31 +2,64 @@ import twint
 import sqlite3
 import pandas as pd
 from datetime import datetime, timedelta
+from tqdm import tqdm
 
-games_details = pd.read_csv("./games_details.csv")
 games = pd.read_csv("./games.csv")
 teams = pd.read_csv("./teams.csv")
-one_game = games.sample(1)
-
-home_team_details = teams[teams["TEAM_ID"]== one_game.HOME_TEAM_ID.values[0]]
-visitor_team_details = teams[teams["TEAM_ID"]==one_game.VISITOR_TEAM_ID.values[0]]
-print(home_team_details.info())
-ht = home_team_details.NICKNAME.values[0]
-vt = visitor_team_details.NICKNAME.values[0]
-search_term  = f"({ht} OR {vt})"
-print(visitor_team_details)
-print(search_term)
-
-game_date = pd.to_datetime(one_game.GAME_DATE_EST)
-week_before = game_date - pd.Timedelta(weeks=1)
-week_before = week_before.dt.strftime('%Y-%m-%d %H:%M:%S').values[0]
-game_date = game_date.dt.strftime('%Y-%m-%d %H:%M:%S').values[0]
-print(week_before, game_date)
-
+games["GAME_DATE_EST"] = pd.to_datetime(games["GAME_DATE_EST"])
 config = twint.Config
-# config.Search = "to:MiamiHEAT OR to:HoustonRockets"
-config.To = "@HoustonRockets"
-config.Since = "2019-10-11"
-config.Until = "2019-10-18"
-config
-twint.run.Search(config)
+config.Store_csv = True
+config.Stats=True
+for index, game in tqdm(games.iterrows(), total=games.shape[0]):
+    print(game)
+    game_id = game.GAME_ID
+    home_team_id = game['HOME_TEAM_ID']
+    print(game_id, home_team_id)
+    home_team_details = teams[teams["TEAM_ID"] == game["TEAM_ID_home"]]
+    away_team_details = teams[teams["TEAM_ID"] == game["TEAM_ID_away"]]
+    home_nickname = home_team_details.NICKNAME.values[0]
+    visitor_nickname = away_team_details.NICKNAME.values[0]
+    home_twitter_handle = home_team_details.TWITTER_HANDLE.values[0]
+    visitor_twitter_handle = away_team_details.TWITTER_HANDLE.values[0]
+    game_date = game.GAME_DATE_EST
+    week_before = game_date - pd.Timedelta(weeks=1)
+    game_date_as_string = game_date.strftime('%Y-%m-%d %H:%M:%S')
+    week_before_as_string = week_before.strftime('%Y-%m-%d %H:%M:%S')
+    config.Since = week_before_as_string
+    config.Until = game_date_as_string
+    
+    config.To = home_twitter_handle
+    config.Output = f"tweets/{game_id}_{home_nickname}.csv"
+    twint.run.Search(config)
+
+    config.To = visitor_twitter_handle
+    config.Output = f"tweets/{game_id}_{visitor_nickname}.csv"
+    twint.run.Search(config)
+
+# home_team_details = teams[teams["TEAM_ID"]== one_game.HOME_TEAM_ID.values[0]]
+# visitor_team_details = teams[teams["TEAM_ID"]==one_game.VISITOR_TEAM_ID.values[0]]
+# print(home_team_details.info())
+# ht = home_team_details.TWITTER_HANDLE.values[0]
+# vt = visitor_team_details.TWITTER_HANDLE.values[0]
+# search_term  = f"({ht} OR {vt})"
+# print(search_term)
+
+# game_date = pd.to_datetime(one_game.GAME_DATE_EST)
+# week_before = game_date - pd.Timedelta(weeks=1)
+# week_before = week_before.dt.strftime('%Y-%m-%d %H:%M:%S').values[0]
+# game_date = game_date.dt.strftime('%Y-%m-%d %H:%M:%S').values[0]
+# print(week_before, game_date)
+
+# config = twint.Config
+# config.To = ht
+# config.Since = week_before
+# config.Until = game_date
+# config.Store_csv = True
+# config.Stats=True
+# # config.Popular_tweets = True
+# config.Output = "tweets.csv"
+# twint.run.Search(config)
+
+# config.To = vt
+# twint.Output = "tweets2.csv"
+# twint.run.Search(config)
